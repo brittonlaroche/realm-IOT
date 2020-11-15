d76469ad7848d61a30c9ce8c91b61f20498657f9
Britton LaRoche <32334829+brittonlaroche@users.noreply.github.com>
2020-11-14 22:25:37 -0600

    Create turnOffWifi.py

---
+++ python/turnOffWifi.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+print("Shutting down wifi for 60 seconds")
+cmd = 'sudo ifconfig wlan0 down'
+os.system(cmd)
+
+time.sleep(60)
+
+cmd = 'sudo ifconfig wlan0 up'
+os.system(cmd)
+print("wifi back up")
