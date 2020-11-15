f3a545cd682167f21d39311a063a294adc235d05
Britton LaRoche <32334829+brittonlaroche@users.noreply.github.com>
2020-11-14 22:24:34 -0600

    Create readTemp.py

---
+++ python/readTemp.py
@@ -0,0 +1,24 @@
+import Adafruit_DHT
+import time
+import requests
+import datetime
+
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4
+
+while True:
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        sensorDate = datetime.datetime.now()
+        ftemp = (temperature * 9/5)+32
+        data = "{\"sensorId\":\"T89176\", \"temperature\":" +str(ftemp) +", \"humidity\":" + str(humidity) +", \"sensorDate\":\"" + str(sensorDate) + "\"}"
+        print(data)
+        url = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/inventory-hhsot/service/Receive-IOT-Data/incoming_webhook/IOT-WH'
+        headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+        response = requests.post(url, data=data, headers=headers )
+        for x in response:
+            print(x)
+
+    else:
+        print("Sensor failure. Check wiring.");
+    time.sleep(3);   
